fc7271be70e25cc4668575a87dbaf7c0c6eaf234
Pedro Santana <pedro.santana9905@gmail.com>
2025-09-01 11:11:33 -0300

    adicionando as funcionalidades com JavaScript

---
+++ SPA puro/script.js
@@ -0,0 +1,20 @@
+
+function exibirPagina(paginaID) {
+    const paginas= document.querySelectorAll('.pagina');
+    paginas.forEach(pagina => pagina.classList.remove('ativo'))
+
+    const paginaParaExibir = document.getElementById(paginaID);
+    
+    paginaParaExibir.classList.add('ativo')
+}
+
+
+document.getElementById('irParaPagina2').addEventListener('click', 
+function(){
+    exibirPagina('pagina2')
+})
+
+document.getElementById('irParaPagina1').addEventListener('click', 
+function(){
+    exibirPagina('pagina1')
+})
